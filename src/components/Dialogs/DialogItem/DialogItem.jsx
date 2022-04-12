f0a28b0c9b6e13f3ed249ecbf13e638b92c9e7d4
Anastasiya <anaborki@gmail.com>
2022-04-12 14:59:09 +0300

    create component DialogItem and Message

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./../Dialogs.module.css";
+
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+  
+  return (
+    <div className={s.dialog}>
+      <NavLink
+        to={path}
+        className={({ isActive }) => 
+          isActive ? s.activeDialog : s.unActiveDialog
+        }
+      >
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+
+  
+export default DialogItem;
