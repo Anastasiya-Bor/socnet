bdef932aa4b92e09b4994f2be3216a1117604178
Anastasiya <anaborki@gmail.com>
2022-05-13 13:22:08 +0300

    del render, add store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import {addPost} from "./redux/state";
-import { rerenderEntireTree } from "./render";
-import state from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/state";
 
-rerenderEntireTree(state);
+
+
+ let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <App state={state} addPost={store.addPost.bind(store)} 
+                      upDateNewPostText={store.upDateNewPostText.bind(store)}/>,
+    document.getElementById("root")
+  );
+};
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,48 +1,61 @@
-import { rerenderEntireTree } from "../render";
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likeCount: 11 },
-      { id: 2, message: "It is my first post", likeCount: 65 },
-      { id: 3, message: "It is my second post", likeCount: 15 },
-    ],
-    newPostText: 'it-kamasutra'
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likeCount: 11 },
+        { id: 2, message: "It is my first post", likeCount: 65 },
+        { id: 3, message: "It is my second post", likeCount: 15 },
+      ],
+      newPostText: 'it-kamasutra'
+    },
+  
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Maksim G" },
+        { id: 3, name: "Ilon M" },
+      ],
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" },
+      ],
+    },
+    sidebar: {}
+  },
+  getState() {
+    return this._state;
   },
 
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Dimych" },
-      { id: 2, name: "Maksim G" },
-      { id: 3, name: "Ilon M" },
-    ],
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Yo" },
-    ],
+  _callSubscriber() {
+    console.log('state changed')
   },
-  sidebar: {}
-};
 
-// window.state = state;
-
-export let addPost = () => {
+  addPost () {
  
-  let newPost = {
-    id: 5, 
-    message: state.profilePage.newPostText, 
-    likeCount: 0
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
+    let newPost = {
+      id: 5, 
+      message: this._state.profilePage.newPostText, 
+      likeCount: 0
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+
+  upDateNewPostText (newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe (observer) {
+    this._callSubscriber = observer; 
+  }
 }
 
-export let upDateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
+export default store;
 
-export default state;
+
+window.store = store;
+
   
